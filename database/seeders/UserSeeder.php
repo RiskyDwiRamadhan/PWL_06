eed764d0aa8024fa175921dd5ed550c9b0f290d7
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-03-16 15:31:16 +0700

    Menambah Nilai awal

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Administrator',
             'email' => 'admin@admin.com',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('password')
         ]);
     }
 }
