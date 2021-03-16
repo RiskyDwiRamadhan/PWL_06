ea6448549f79ffd764810e8d39aa5ca1794539ea
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-03-16 15:25:42 +0700

    Menambahkan kolom username pada table users

--- database/migrations/2021_03_16_081555_tambah_kolom_ditabel_user.php
+++ database/migrations/2021_03_16_081555_tambah_kolom_ditabel_user.php
@@ -13,7 +13,9 @@ class TambahKolomDitabelUser extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username', 20)->after('id')->nullable()->unique();
+        });
     }
 
     /**
@@ -23,6 +25,8 @@ class TambahKolomDitabelUser extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColum('username');
+        });
     }
 }
